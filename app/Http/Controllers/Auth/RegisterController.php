ec503fe56b980ef051d00b3271c956d2ea0b05d3
Juliano Pimentel <juliano_pimentel@live.com>
2021-12-18 04:39:43 -0300

    outro ajuste no email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,8 +7,6 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Notifications\WelcomeEmail;
-
 
 class RegisterController extends Controller
 {
