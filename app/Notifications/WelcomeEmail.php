d06e5cdb943885e3cdecc410c1423b8fa3e0668b
Juliano Pimentel <juliano_pimentel@live.com>
2021-12-18 04:24:17 -0300

    Update WelcomeEmail.php

--- app/Notifications/WelcomeEmail.php
+++ app/Notifications/WelcomeEmail.php
@@ -6,6 +6,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use App\Models\EmailTemplate;
+
 
 class WelcomeEmail extends Notification
 {
@@ -40,10 +42,9 @@ class WelcomeEmail extends Notification
      */
     public function toMail($notifiable)
     {
+        $template = EmailTemplate::find(4);
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->line($template->subject);
     }
 
     /**
