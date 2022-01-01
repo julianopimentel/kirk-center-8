bf41d977f771983f9c7b3b34bcdbf110286749d1
Juliano Pimentel <juliano_pimentel@live.com>
2022-01-01 16:39:10 -0300

    Update homepage.blade.php

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -58,7 +58,7 @@
                             </div>
                             <div class="card-footer px-3 py-2">
                                 <a class="btn-block text-muted d-flex justify-content-between align-items-center"
-                                    href="{{ url('notes') }}">
+                                    href="{{ url('people') }}">
                                     <span class="small font-weight-bold">View More</span>
                                 </a>
                             </div>
@@ -98,7 +98,7 @@
                             </div>
                             <div class="card-footer px-3 py-2">
                                 <a class="btn-block text-muted d-flex justify-content-between align-items-center"
-                                    href="{{ url('calendar') }}">
+                                    href="{{ url('calender') }}">
                                     <span class="small font-weight-bold">View More</span>
                                 </a>
                             </div>
@@ -118,7 +118,7 @@
                             </div>
                             <div class="card-footer px-3 py-2">
                                 <a class="btn-block text-muted d-flex justify-content-between align-items-center"
-                                    href="{{ url('notes') }}">
+                                    href="{{ url('message') }}">
                                     <span class="small font-weight-bold">View More</span>
                                 </a>
                             </div>
