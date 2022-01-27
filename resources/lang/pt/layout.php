746cd0d1d1b25715f4b94fcb80e93f25f0723bf6
Juliano Pimentel <juliano_pimentel@live.com>
2022-01-26 23:19:06 -0300

    linguagem menu

---
+++ resources/lang/pt/layout.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Geral Language
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    |
+    */
+
+    //menu
+    'home' => 'home',
+    'timeline' => 'Timeline',
+    'dashboard' => 'Dashboard',
+    'people' => 'Pessoas',
+    'group' => 'Grupos',
+    'message' => 'Mural de Recados',
+    'prayer' => 'Pedido de Oração',
+    'calender' => 'Calendário',
+    'documents' => 'Documentos',
+    'financial' => 'Financeiro',
+    'report' => 'Relatórios',
+    'configuration' => 'Configurações',
+
+    //sidebar
+    'account' => 'Contas',
+    'license' => 'Licença',
+    'available' => 'Disponível',
+    'used' => 'Usado',
+    'name' => 'Nome',
+    'type' => 'Tipo',
+    'action' => 'Ações',
+    'select' => 'Selecione uma conta',
+    'doc' => 'CNPJ',
+
+    //types
+    'Church' => 'Igreja',
+    'Business' => 'Empresarial',
+    'Education' => 'Educacional',
+    'Government' => 'Governamental',
+];
