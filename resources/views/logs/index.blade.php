6fa91913390f4b4e43a7a30be32f9e55c272a905
Juliano Pimentel <juliano_pimentel@live.com>
2022-02-16 20:57:28 -0300

    view json

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.base')
 @section('content')
-<div class="container-fluid">
-    <div class="fade-in">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    
-                      <div class="card-header"><h6>Logs</h6></div>
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+
+                        <div class="card-header">
+                            <h6>Logs</h6>
+                        </div>
                         <div class="box-body">
                             <div class="col-sm-12 col-md-10 col-lg-8 col-xl-12">
                                 <div class="card">
@@ -14,13 +16,21 @@
                                         <br>
                                         <h4>Request:</h4>
                                         <p> </p>
-                                        <div class="form-floating">
-                                            <pre> {{$logs->manipulations}}</pre>
-                                          </div>
+                                        @php
+                                            
+                                            $jsonobj = $logs->manipulations;
+                                            
+                                            $obj = json_decode($jsonobj);
+                                            
+                                            foreach ($obj as $key => $value) {
+                                                echo $key . ' => ' . $value . '<br>';
+                                            }
+                                        @endphp
+                                        <br>
                                         <a class="btn btn-primary" href="{{ route('logs.index') }}">Retornar</a>
-                                      </div>
+                                    </div>
                                 </div>
-                              </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -28,9 +38,8 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
 
 @section('javascript')
-
 @endsection
