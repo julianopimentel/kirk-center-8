08d7704623f9f36fb23cbdf800293aec71d8e2ef
Juliano Pimentel <juliano_pimentel@live.com>
2022-02-22 17:52:16 -0300

    https maps + funcionalidade

--- resources/views/reports/PeopleLoc.blade.php
+++ resources/views/reports/PeopleLoc.blade.php
@@ -1,7 +1,7 @@
 @if ($appPermissao->report_view and $appPermissao->view_people == true)
 @extends('layouts.base')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="http://maps.google.com/maps/api/js"></script>
+<script src="https://maps.google.com/maps/api/js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.js"></script>
 
 
