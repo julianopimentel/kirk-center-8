e73de0f3530a0b8f3e9203b837279da5485a5d64
Juliano Pimentel <juliano_pimentel@live.com>
2022-03-01 15:13:37 -0300

    menu minimal

--- resources/views/layouts/shared/nav-minimal.blade.php
+++ resources/views/layouts/shared/nav-minimal.blade.php
@@ -15,43 +15,54 @@
             <i class="c-icon cil-building c-sidebar-nav-icon"></i>
             {{ __('layout.account') }}</a>
     </li>
-    @if(Auth::user()->menuroles == true)
+    @if(Auth::user()->menuroles == 'admin')
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ url('license') }}">
             <i class="c-icon cil-credit-card c-sidebar-nav-icon"></i>
             {{ __('layout.license') }}</a>
     </li>
     @endif
-    @if (Auth::user()->isAdmin() === true)
+    @if (Auth::user()->isAdmin() == true)
+    <li class="c-sidebar-nav-title">Admin</li>
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ url('users') }}">
             <i class="c-icon cil-user c-sidebar-nav-icon"></i>
             Usuários</a>
     </li>
     <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link" href="{{ url('log') }}">
-            <i class="c-icon cil-reload c-sidebar-nav-icon"></i>
-            Logs</a>
+        <a class="c-sidebar-nav-link" href="{{ url('integrador') }}">
+            <i class="c-icon cil-institution c-sidebar-nav-icon"></i>
+            Integradores</a>
+    </li>
+    <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="{{ url('transations') }}">
+            <i class="c-icon cil-credit-card c-sidebar-nav-icon"></i>
+            Transações</a>
     </li>
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ route('blog.post') }}">
-            <i class="c-icon cil-notes c-sidebar-nav-icon"></i>
+            <i class="c-icon cil-newspaper c-sidebar-nav-icon"></i>
             Add Post</a>
     </li>
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ route('bread.index') }}">
-            <i class="c-icon cil-notes c-sidebar-nav-icon"></i>
+            <i class="c-icon cil-browser c-sidebar-nav-icon"></i>
             Bread</a>
     </li>
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ route('mail.index') }}">
-            <i class="c-icon cil-notes c-sidebar-nav-icon"></i>
+            <i class="c-icon cil-at c-sidebar-nav-icon"></i>
             Mail</a>
     </li>
-
+    <li class="c-sidebar-nav-title">System</li>
+    <li class="c-sidebar-nav-item">
+        <a class="c-sidebar-nav-link" href="{{ url('log') }}">
+            <i class="c-icon cil-sync c-sidebar-nav-icon"></i>
+            Logs</a>
+    </li>
     <li class="c-sidebar-nav-item">
         <a class="c-sidebar-nav-link" href="{{ url('/clear-cache-all-057878545112') }}">
-            <i class="c-icon cil-notes c-sidebar-nav-icon"></i>
+            <i class="c-icon cil-terminal c-sidebar-nav-icon"></i>
             Limpar cache</a>
     </li>
     @endif
