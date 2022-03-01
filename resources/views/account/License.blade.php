2b2002dfc63c0de39a4dcb10dbec1c76e59bca0a
Juliano Pimentel <juliano_pimentel@live.com>
2022-03-01 15:17:01 -0300

    Update License.blade.php

--- resources/views/account/License.blade.php
+++ resources/views/account/License.blade.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="row">
                     <div class="form-group col-sm-4 col-md-6 col-lg-6 col-xl-6">
-                        @if (Auth::user()->license - $countinst >= 1 || Auth::user()->isAdmin())
+                        @if (Auth::user()->license - $countinst >= 1 || Auth::user()->isAdmin() == true)
                             <a href="{{ route('account.create') }}" class="btn btn-primary m-2" title="Adicionar"> <i
                                     class="c-icon c-icon-sm cil-plus"></i></a>
                         @endif
